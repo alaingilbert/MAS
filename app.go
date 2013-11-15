8be88142728af2e844e7f4021d30cbbf3a8328ab
Alain Gilbert <alain.gilbert.15@gmail.com>
2013-11-15 00:55:22 -0500

    app in app.go

--- app.go
+++ app.go
@@ -4,17 +4,96 @@ package main
 import (
   //"mas/core"
   //"mas/draw"
+  "fmt"
+  "image/png"
+  "net/http"
+  "mas/core"
   "mas/license"
   "mas/logger"
-  "mas/web"
   "mas/worker"
   "os"
   "runtime"
   "time"
+  "strconv"
+  "html/template"
+  "io/ioutil"
+  "mas/draw"
 )
 
 
 var s_Logger logger.Logger = logger.NewLogger(logger.INFO | logger.DEBUG)
+
+var world *core.World = core.NewWorld("/Users/agilbert/Desktop/minecraft/world")
+var theme map[byte]core.Block = core.LoadTheme("default")
+
+
+const PORT int = 8000
+
+
+func TileHandler(w http.ResponseWriter, req *http.Request) {
+  x, _ := strconv.Atoi(req.URL.Query()["x"][0])
+  y, _ := strconv.Atoi(req.URL.Query()["y"][0])
+  z, _ := strconv.Atoi(req.URL.Query()["z"][0])
+  fileName := fmt.Sprintf("tiless/r.%s.%s.png", x ,z)
+  s_Logger.Debug("Serve tile x:", x, "y:", y, "z:", z)
+  file, err := os.Open(fileName)
+  if err != nil {
+    img := draw.RenderTile(x, y, z, world, theme)
+    png.Encode(w, img)
+    //http.NotFound(w, req)
+    return
+  }
+  defer file.Close()
+  w.Header().Set("Content-type", "image/png")
+  img, err := png.Decode(file)
+  png.Encode(w, img)
+}
+
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+  tmpl, err := template.ParseFiles("templates/index.html")
+  if err != nil {
+    fmt.Println(err)
+  }
+  tmpl.Execute(w, map[string] string {"title": "Test title"})
+}
+
+
+func LeafletJsHandler(w http.ResponseWriter, req *http.Request) {
+  file, _ := ioutil.ReadFile("static/js/leaflet.js")
+  w.Header().Set("Content-Type", "application/x-javascript")
+  w.Write(file)
+}
+
+func LeafletCssHandler(w http.ResponseWriter, req *http.Request) {
+  file, _ := ioutil.ReadFile("static/css/leaflet.css")
+  w.Header().Set("Content-Type", "text/css")
+  w.Write(file)
+}
+
+func LeafletZoomInHandler(w http.ResponseWriter, req *http.Request) {
+  file, _ := ioutil.ReadFile("static/css/images/zoom-in.png")
+  w.Header().Set("Content-Type", "image/png")
+  w.Write(file)
+}
+
+func LeafletZoomOutHandler(w http.ResponseWriter, req *http.Request) {
+  file, _ := ioutil.ReadFile("static/css/images/zoom-out.png")
+  w.Header().Set("Content-Type", "image/png")
+  w.Write(file)
+}
+
+
+func Server() {
+  s_Logger.Debug("Start web server")
+  http.HandleFunc("/tile/", TileHandler)
+  http.HandleFunc("/static/css/leaflet.css", LeafletCssHandler)
+  http.HandleFunc("/static/css/images/zoom-in.png", LeafletZoomInHandler)
+  http.HandleFunc("/static/css/images/zoom-out.png", LeafletZoomOutHandler)
+  http.HandleFunc("/static/js/leaflet.js", LeafletJsHandler)
+  http.HandleFunc("/", HomeHandler)
+  http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
+}
 
 
 func main() {
@@ -48,7 +127,7 @@ func main() {
   workerPool := worker.NewWorkerPool(numCPU)
 
   // start webserver
-  web.Server()
+  Server()
 
   //startTime := time.Now()
   //world := core.NewWorld("/Users/agilbert/Desktop/minecraft/world")
