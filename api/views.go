3a293ee8b3b3b327d798c5d874b605b35f9e9b59
alaingilbert <alain.gilbert.15@gmail.com>
2013-11-20 12:09:58 -0500

    clean up skins

--- api/views.go
+++ api/views.go
@@ -39,30 +39,23 @@ func PlayerIconHandler(w http.ResponseWriter, req *http.Request, params martini.
 
   scale := 4
   is := 8 * scale
+  var img image.Image
+
   if resp.StatusCode != 200 {
     file, err := os.Open("./public/skins/default.png")
     if err != nil {
       fmt.Println(err)
     }
     defer file.Close()
-    img, _ := png.Decode(file)
-    img = resize.Resize(uint(64 * scale), 0, img, resize.NearestNeighbor)
-    dest := image.NewRGBA(image.Rect(0, 0, is, is))
-    draw.Draw(dest, image.Rect(0, 0, is, is), img, image.Point{scale * 8, scale * 8}, draw.Src)
-    png.Encode(w, dest)
-    return
+    img, _ = png.Decode(file)
+  } else {
+    img, _ = png.Decode(resp.Body)
+    resp.Body.Close()
   }
 
-  //w.Header().Set("Content-Length", fmt.Sprint(resp.ContentLength))
-  //w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
   w.Header().Set("Content-type", "image/png")
-
-  img, _ := png.Decode(resp.Body)
   img = resize.Resize(uint(64 * scale), 0, img, resize.NearestNeighbor)
   dest := image.NewRGBA(image.Rect(0, 0, is, is))
   draw.Draw(dest, image.Rect(0, 0, is, is), img, image.Point{scale * 8, scale * 8}, draw.Src)
-
-  //io.Copy(w, dest)
   png.Encode(w, dest)
-  resp.Body.Close()
 }
